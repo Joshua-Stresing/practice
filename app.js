ba253fd165962f1d864bfdcbe7df18fd4cfbaf71
Joshua Stresing <pyrotorch69@yahoo.com>
2022-02-01 10:45:02 -0800

    startup

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 // import functions and grab DOM elements
-
+import { animals } from './animals.js';
+import { renderAnimal } from './utils.js';
+console.log(animals);
+const animalList = document.getElementById('animal-list');
 // let state
-
+for (let animal of animals) {
+    const div = renderAnimal(animal);
+    animalList.append(div);
+}
 // set event listeners 
   // get user input
   // use user input to update state 
---
+++ soda.js
@@ -0,0 +1,9 @@
+export const sodas = [
+    {
+        name: 'Coke',
+        nutrition: {
+            calories: 150,
+            servingSize: '100mL',
+        },
+    },
+];
